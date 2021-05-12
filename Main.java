5be4eebfd77094b83e272924335da80a858e353c
Emily Flores <gfemily_22@hotmail.com>
2021-05-12 10:17:18 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+      int myNum = 5;
+  float myFloatNum = 5.99f;
+  char myLetter = 'D';
+  boolean myBool = true;
+  String myText = "Hello";
+  final int= 8; //no se pueden hacer cambios.
+  int=6; //generará un error.
+  
+  int x= 1, y=2, z=3;
+  System.out.println(x + y + z);
+  //Imprimirá el resultado de la suma de x, y y z.
+    }
+  }
